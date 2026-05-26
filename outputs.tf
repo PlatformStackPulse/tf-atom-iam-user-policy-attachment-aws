97d78bc14f6e388a43a0496e0333ac960e543344
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 20:37:33 +0100

    feat: implement tf-atom-iam-user-policy-attachment-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "user" {
+  description = "Name of the user the policy is attached to"
+  value       = try(aws_iam_user_policy_attachment.this[0].user, null)
+}
+
+output "policy_arn" {
+  description = "ARN of the attached policy"
+  value       = try(aws_iam_user_policy_attachment.this[0].policy_arn, null)
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,17 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "user_name" {
+  description = "Name of the IAM user to attach the policy to"
+  type        = string
+  validation {
+    condition     = length(var.user_name) > 0
+    error_message = "user_name must not be empty."
+  }
+}
+
+variable "policy_arn" {
+  description = "ARN of the IAM policy to attach"
+  type        = string
+  validation {
+    condition     = can(regex("^arn:aws:iam::", var.policy_arn))
+    error_message = "policy_arn must be a valid IAM policy ARN."
+  }
+}
